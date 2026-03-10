42af289402f9f51df25d1c6d873d3e202ecaaa0b
Kurt <k.brearley4387@student.leedsbeckett.ac.uk>
2026-03-10 20:49:09 +0000

    Added UTF-8 encoding for App.jsx to allow emojis to show on the frontend

--- front/frontend/src/App.jsx
+++ front/frontend/src/App.jsx
@@ -1,18 +1,24 @@
-import { useEffect } from "react";
+﻿import { useEffect, useState } from "react";
 
 function App() {
+    const [habits, setHabits] = useState([]);
 
     useEffect(() => {
-        fetch("http://localhost:5000/")
-            .then(res => res.text())
-            .then(data => console.log(data))
+        fetch("http://localhost:5000/habits")
+            .then(res => res.json())
+            .then(data => setHabits(data))
             .catch(err => console.error(err));
     }, []);
 
     return (
         <div>
             <h1>Habit Tracker</h1>
-            <p>Check the browser console for backend response.</p>
+
+            {habits.map(habit => (
+                <p key={habit.id}>
+                    {habit.name} - {habit.completed ? "✅" : "❌"}
+                </p>
+            ))}
         </div>
     );
 }
